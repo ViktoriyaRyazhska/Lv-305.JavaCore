c926064ce34da9bc69a25a7b98b6c6972f4a1efe
Vladlen Onopko <vonoptc@training.local>
2018-03-20 17:56:44 +0200

    update homework directory

---
+++ lesson03/practical/Employee.java
@@ -0,0 +1,75 @@
+package lesson03.practical;
+
+public class Employee {
+    private String name;
+    private double rate;
+    private double hours;
+    private static double totalSum;
+
+    String getName() {
+        return name;
+    }
+
+     void setName(String name) {
+        this.name = name;
+    }
+
+     double getRate() {
+        return rate;
+    }
+
+     void setRate(double rate) {
+        this.rate = rate;
+    }
+
+     double getHours() {
+        return hours;
+    }
+
+     void setHours(double hours) {
+        this.hours = hours;
+    }
+
+     static double getTotalSum() {
+        return totalSum;
+    }
+
+     static void setTotalSum(double totalSum) {
+        Employee.totalSum = totalSum;
+    }
+    public Employee(){
+    }
+
+    public Employee(String name, double rate) {
+        this.name = name;
+        this.rate = rate;
+    }
+
+    public Employee(String name, double rate, double hours) {
+        this.name = name;
+        this.rate = rate;
+        this.hours = hours;
+    }
+     double salary(){
+        return getRate()*getHours();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name : %s. Rate : %s. Hours : %s.", getName(),String.format("%.2f",getRate()),String.format("%.2f",getHours()));
+    }
+     void changeRate(double rate){
+        setRate(rate);
+    }
+     double bonuses(){
+        return 0.1*salary();
+    }
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Mike",10.5,8);
+        Employee employee2 = new Employee("John",15,9);
+        Employee employee3 = new Employee("Mark",22.2,8.5);
+        System.out.println(employee1+"\n"+employee2+"\n"+employee3);
+    }
+}
+
