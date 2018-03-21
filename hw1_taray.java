626506c8c8c0157ab3d96bd97fb959877b82a728
taras16 <taras.yurychko@gmail.com>
2018-03-22 00:25:20 +0200

    update

--- hw1_taray.java
+++ hw1_taray.java
@@ -24,8 +24,32 @@ public class Flower {
 	   	    System.out.println("Where are you live, "+ name);
 	    String adress = flow.next(); 
 	    	System.out.println(name + " live in "+ adress);
+
+// call to country
+	    	Scanner call = new Scanner(System.in);
+			
+			System.out.println( " input money  min C1 country: " );
+			int c1 = call.nextInt(); 
+			System.out.println("input money per min C2  country : ");
+			int c2 =call.nextInt(); 
+			System.out.println("input money per min C3 country:  "); 
+			int c3 = call.nextInt();
+			
+		 
+			 
+			System.out.println("how long C1 : " );
+			int t1 = call.nextInt();
+			System.out.println ("how long C2 : " ); 
+			int t2 = call.nextInt(); 
+			System.out.println ("how long C2 : " ); 
+			int t3 = call.nextInt(); 
+			
+			
+			System.out.println("C1 call = " + c1 * t1 ); 
+			System.out.println("C1 call = " + c2 * t2 ); 
+			System.out.println("C1 call = " + c3 * t3 ); 
+			System.out.println("All calls = " + (c1 * t1 + c2 * t2 + c3 *c2)); 
 		
-	    	
 			
 	}
 
