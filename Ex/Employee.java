f8269d18b47407bbfa22c506c91825948f0eb48e
Vasia Pupkin <vpupkin@mail.com>
2018-03-23 00:24:49 +0200

    1

--- Ex/Employee.java
+++ Ex/Employee.java
@@ -5,9 +5,46 @@ public class Employee {
 	private String name;
 	private int rate;
 	private int hours;
-	private static int totalSum=0;
-	
-	public Employee() {}
+	private static double totalSum=0;
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		totalSum = totalSum - this.rate * this.hours;
+		this.rate = rate;
+		totalSum = totalSum + this.rate * this.hours;
+
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		totalSum = totalSum - this.rate * this.hours;
+		this.hours = hours;
+		totalSum = totalSum + this.rate * this.hours;
+
+	}
+
+	public static double getTotalSum() {
+		return totalSum;
+	}
+
+	public static void setTotalSum(double totalSum) {
+		Employee.totalSum = totalSum;
+	}
 	
 	public Employee(String name, int rate) {
 		this.name = name;
@@ -18,8 +55,11 @@ public class Employee {
 		this.name = name;
 		this.rate = rate;
 		this.hours = hours;
+		totalSum = totalSum + this.rate * this.hours;
+
 	}
-	
+
+
 	public int getSalary() {
 		return  rate * hours;
 		}
@@ -32,6 +72,7 @@ public class Employee {
 	public String toString() {
 	return "Employee [name=" + name + ", rate=" + rate + ", hours=" + hours +"]";
 	}
+
 	
 
 
