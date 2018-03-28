911d8f1cb6daa03c593e6fc94e4f90b6a74adfcf
vladlenonopko <vladlenonopko@gmail.com>
2018-03-28 14:23:19 +0300

    upd 28

---
+++ src/main/java/lesson07/homework/FiveWordsSentence.java
@@ -0,0 +1,4 @@
+package lesson07.homework;
+
+public class FiveWordsSentence {
+}
---
+++ src/main/java/lesson07/practical/SubstringSecond.java
@@ -0,0 +1,11 @@
+package lesson07.practical;
+
+import java.util.regex.Matcher;
+
+public class SubstringSecond {
+    public static void main(String[] args) {
+        String first = "IT academy";
+        String second = "IT";
+
+    }
+}
