cf2d13317045125519675f085b3fe77c65871728
iryna0706 <37587878+iryna0706@users.noreply.github.com>
2018-03-28 22:10:06 +0300

    Add files via upload
    воно видає ерор

---
+++ Nschalo.java
@@ -0,0 +1,41 @@
+
+public class Nschalo {
+
+	public static void main(String[] args) {
+		float num1 = 4;
+		float num2 = 6;
+		float num3 = -2;
+		
+	if (num1 <= 5) {
+		System.out.println("Number 1 is satisfies");
+	} else {
+		System.out.println("Number 1 is wrong");
+	}
+	if (num1 >= 5)	{
+		System.out.println("Number 1 is satisfies");
+	} else {
+		System.out.println("Number 1 is wrong");
+	}
+	if (num2 <= 5) {
+		System.out.println("Number 2 is satisfies");
+	} else { 
+		System.out.println("Number 2 is wrong");
+	}
+	if (num2 >= 5) {
+		System.out.println("Number 2 is satisfies");
+	} else {
+		System.out.println("Number 2 is wrong");
+	}
+	if (num3 <= 5) {
+		System.out.println("Number 3 is satisfies");
+	} else {
+		System.out.println("Number 3 is wrong");
+	}
+	if (num3 >= 5) {
+		System.out.println("Number 3 is satisfies");
+	} else {
+		System.out.println("Number 3 is wrong");
+	}
+System.out.println();
+}
+}
