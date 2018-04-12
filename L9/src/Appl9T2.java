3fa6fec2d60a46cf76bb4196f3647785335c5cac
Vasia Pupkin <vpupkin@mail.com>
2018-04-12 18:59:29 +0300

    l9

--- L9/src/Appl9T2.java
+++ L9/src/Appl9T2.java
@@ -1,9 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Appl9T2 {
+	   static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		   try {
-		      Plants pl = new Plants("rose", "blue", 5);
+
+			   System.out.println("Input color and type, size");
+			   String col = reader.readLine();
+			   String type = reader.readLine();
+			   int n = Integer.parseInt(reader.readLine());
+			   
+		      Plants pl = new Plants(type, col, n);
+			   
+//		       String color = reader.readLine();
+//		       int size = Integer.parseInt(reader.readLine());
 		      System.out.println(pl);
 		   } catch (ColorException | TypeException e) {
 		      System.err.println(e.getMessage() + "\n");
